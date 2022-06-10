9bff070857c9c03385109fed60c49ef6151b04d9
AtulSikarwal <atulsikarwal5.com>
2022-06-10 12:58:51 +0530

    The code for calculate working Hourse and Monthly Employee Wage

---
+++ UC6_TotalWorkingHourseAndMonthly/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Basicprogram
+{
+
+    class program
+    {
+
+        public static void Main(String[] args)
+        {
+            EmpolyWage Empwage = new EmpolyWage();
+            Empwage.CalculateWage();
+        }
+    }
+}
