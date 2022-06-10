bf44e8efb6e4f8de0c268a27ca481fae570f58fe
AtulSikarwal <atulsikarwal5.com>
2022-06-10 12:03:48 +0530

    The code fir Employee is Present or Not

---
+++ UC1_EmployeePresentORNot/EmpolyWage.cs
@@ -0,0 +1,32 @@
+﻿namespace Basicprogram
+
+{
+    internal class EmployeeWage
+    {
+
+      
+        public const int IsAbsnt = 0;
+        public const int IsPresent= 1;
+       
+        public int IsEmployeePresent()
+        {
+            return new Random().Next(0, 2);
+        }
+         public void CalculateWage()
+        {
+        switch(IsEmployeePresent())
+            {
+                case IsAbsnt:
+                    Console.WriteLine("Employee is Not Present");
+                    break;
+                case IsPresent:
+                    Console.WriteLine("Employee is Present");
+                    break ;
+            }
+
+
+        }
+            }
+}
+
+    
