6a66d34d152ba90e014fdabc58c64c7ecb3c1250
AtulSikarwal <atulsikarwal5.com>
2022-06-10 12:50:18 +0530

    The code for Employee Monthly Wage

---
+++ UC5_EmpWageForMonth/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Basicprogram
+{
+
+    class program
+    {
+
+        public static void Main(String[] args)
+        {
+            EmployeeWage Empwage = new EmployeeWage();
+            Empwage.CalculateWage();
+        }
+    }
+}
