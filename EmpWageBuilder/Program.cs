1e3726116eb3c295e31555e6bf3c44f21c313990
AtulSikarwal <atulsikarwal5.com>
2022-06-17 12:10:04 +0530

    The code for EmpWageBuilder to get total wage by Company

---
+++ EmpWageBuilder/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Basicprogram
+{
+
+    class program
+    {
+
+        public static void Main(String[] args)
+        {
+            EmpolyWage Empwage = new EmpolyWage(3);
+            Empwage.AddCompany("Dell", 23, 22, 11, 44, 33);
+            Empwage.AddCompany("Tcs", 3, 2, 11, 4, 3);
+            Empwage.CalculateWage("Tcs");
+            Empwage.CalculateWage("Dell");
+            Empwage.DisplayArray();
+
+            Console.Write("Enter the name of Company you want to see the total wage of :");
+            string Name = Console.ReadLine();
+            Empwage.View_Wage(Name);
+
+
+        }
+    }
+}
