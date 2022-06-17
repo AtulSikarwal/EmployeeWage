1a6b18bf1f2c4d703718507ff1868fcbd869c3d4
AtulSikarwal <atulsikarwal5.com>
2022-06-17 11:59:25 +0530

    The code for Store DailyWage And TotalWage.

---
+++ StoreDaily_And_TotalWage/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Basicprogram
+{
+
+    class program
+    {
+
+        public static void Main(String[] args)
+        {
+            EmpolyWage Empwage = new EmpolyWage(3);
+            Empwage.AddCompany("Dell", 23, 22, 11, 44, 33);
+            Empwage.AddCompany("Tcs", 3, 2, 11, 4, 3);
+            Empwage.CalculateWage("Tcs");
+            Empwage.CalculateWage("Dell");
+            Empwage.DisplayArray();           
+
+
+
+        }
+    }
+}
